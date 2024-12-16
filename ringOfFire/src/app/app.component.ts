0c9f60deba3ad13e8fc0aeebfc1f0a30c33576d5
SandorBalazsi <166751447+SandorBalazsi@users.noreply.github.com>
2024-12-16 04:59:22 +0100

    first playable version

--- ringOfFire/src/app/app.component.ts
+++ ringOfFire/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { MatButtonModule} from '@angular/material/button';
 import { RouterOutlet } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatDialogModule} from '@angular/material/dialog';
+import { GameInfoComponent } from './game-info/game-info.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, MatDividerModule, MatIconModule, MatDialogModule, GameInfoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
